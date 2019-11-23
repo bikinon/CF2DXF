e34fc36c930993a4e9890617e7081724c5137002
bikinon <bikinon@users.noreply.github.com>
2019-11-23 10:47:56 +0000

    Initial upload
    This is the initial upload of the files, it includes a description of the CF2 file format and a couple of example files.

---
+++ convertCls.java
@@ -0,0 +1,216 @@
+package cf2dxf;
+
+import java.lang.*;
+import java.io.*;
+
+import javax.swing.JOptionPane;
+
+public class convertCls extends dxf12objects{
+
+	public String cf2File;
+	public String dxfFile;
+	public int layer;
+	public double ang;
+        public double radius;
+    
+   
+	public void convertDxf() {
+		// ***** Draw samples as HPGL files ****
+		// ***** TG - 28/10/97 ****************		THIS VERSION IN JAVA 6/12/2016... mean while 20 years on! :-)
+		
+                double tmpAng;
+		int dirc;
+                String txt = "";
+
+		int  s1,s2,index, auxLT;
+		double  sang=0,eang=0;
+		String cmd="";
+		String[] thevals = new String[15];
+		double v1=0,v2=0,v3=0,v4=0,v5=0,v6=0;
+		
+		dxf += dxf_header12();
+				
+		try (BufferedReader br = new BufferedReader(new FileReader(cf2File))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+
+		    if (line.length() > 3) {
+                        cmd = line.substring(0, 2);
+                        if (line.contains(cmd)) {
+                            thevals = line.split(",", -1);
+                        }
+                    }
+                    
+		      System.out.println(cmd + "*" + line);
+		      if (cmd.equals("L,")) {
+		            v1=Double.parseDouble(thevals[4]); // start X
+		            v2=Double.parseDouble(thevals[5]); // start Y
+		            v3=Double.parseDouble(thevals[6]) - v1; // End X
+		            v4=Double.parseDouble(thevals[7]) - v2; // End Y
+		            layer = Integer.parseInt(thevals[2]);  // Layer / pen
+		            auxLT=Integer.parseInt(thevals[3]); // Auxillery line type
+
+		        	this.xabs = v1;
+		        	this.yabs = v2;
+		            Line(v3, v4, Integer.toString(this.layer));
+		      }// if cmd='L,'
+		      
+		      if (cmd.equals("A,")) {
+                            //0 =                                                           - is the pointage in points (1/72 inches).
+		            v1=Double.parseDouble(thevals[4]); // start X                   - the start coordinate of the arc. X
+		            v2=Double.parseDouble(thevals[5]); // start Y                   - the start coordinate of the arc. Y
+		            v3=Double.parseDouble(thevals[6]); // End X                     - the end coordinate of the arc X
+		            v4=Double.parseDouble(thevals[7]); // End Y                     - the end coordinate of the arc Y
+		            v5=Double.parseDouble(thevals[8]); // Centre X
+		            v6=Double.parseDouble(thevals[9]); // Centre Y
+		            dirc =  Integer.parseInt(thevals[10]); // Direction of rotation
+		            layer = Integer.parseInt(thevals[2]);  // Layer / pen           - is the common file linetype
+		            auxLT = Integer.parseInt(thevals[3]); // Auxillery line type    - the common file auxiliary linetype
+
+		            if ((v1==v3) && (v2==v4)) { // Circle
+		                // HPGLcircle(v1, v2, v5, v6, dirc);
+                                 find_Ang_Rad(v1,v2,v5,v6,dirc);
+                                circle(v5, v6, radius, Integer.toString(layer), layer);
+		            }
+		            else {// Arc
+		            	sang = find_Ang_Rad(v1,v2,v5,v6,dirc);
+		                eang = find_Ang_Rad(v3,v4,v5,v6,dirc);
+		                ang = sang - eang;
+
+		                if (dirc == -1) { // eang > sang && 
+                                //    ang = 360  - ang;
+                                    tmpAng = sang;
+                                    sang = eang;
+                                    eang = tmpAng;
+                                }
+                                arc2(v5, v6, radius, sang, eang, 0, 0, String.valueOf(layer));
+		        } // (v1=v3) and (v2=v4)
+		    } // if cmd='A,'
+
+
+			  if (cmd.equals("T,")) {
+                              //0,1,2,3,4          ,5          ,6        ,7        ,8
+                              //T,0,8,0,116.1061660,405.4294330,0.0000000,8.0000000,5.9946713
+		            v1=Double.parseDouble(thevals[4]); // start X                   - the bottom left position of the text design X
+		            v2=Double.parseDouble(thevals[5]); // start Y                   - the bottom left position of the text design Y
+		            v3=Double.parseDouble(thevals[6]); // Text angle                - the rotation about x, y of the text design.
+		            v4=Double.parseDouble(thevals[7]); // Size of Text              - the height of the characters in mm.
+		            v5=Double.parseDouble(thevals[8]); // No idea???                - the width of the characters including the inter-character gap in mm.
+		            layer = Integer.parseInt(thevals[2]);  // Layer / pen           - the common file linetype
+		            auxLT = Integer.parseInt(thevals[3]); // Auxillery line type    - the common file auxiliary linetype
+                            
+                            txt = br.readLine();
+				   System.out.println(cmd + "*" + line);
+                            TextInsert(v1, v2, txt, String.valueOf(v4), String.valueOf(layer));
+			  }
+                          
+                    cmd = "";      
+					  
+		  }  // while
+
+		} catch (IOException e) {
+			e.printStackTrace(); // auto close of file
+		}
+		
+	    // dxf += dxf_footer12(); WONT PRINT!
+	    dxf +="  0\r\n";
+	    dxf +="ENDSEC\r\n";
+	    dxf +="  0\r\n";
+	    dxf +="EOF\r\n";
+	    
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(dxfFile))) {
+			// String content = "This is the content to write into file\n";
+			bw.write(dxf);
+			//bw.close(); // no need to close it.
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+                String msg = "CONVERTED:\n" + cf2File + "\n\nConverted to DXF\n";
+                msg += "This program performs a standard Layer conversion from the CF2 spec.\n";
+                msg += "It doesn't follow that whoever created your file did the same.\n";
+                msg += "Note - Rarer commands such as Scale\nare not dealt with in this program.";
+		JOptionPane.showMessageDialog(null, msg, "CF2 to DXF by Tim Gathercole 1997-2019", JOptionPane.INFORMATION_MESSAGE ); //  28/10/1997 - 21/03/2019
+
+		
+	}
+
+	
+
+protected double find_Ang_Rad(double absX, double absY, double xc, double yc, int dirc) { //Var absX,absY,xc,Yc,ang : Single; Var dirc : Integer);
+/*  absX    - start X
+    absY    - start Y
+    xc      - Centre X
+    yc      - Centre Y
+    dirc    - Direction of rotation
+    */
+// rtnAng - Var used to RETURN Angle of movement
+// radius - Global used to return RADIUS
+    double rtnAng = 0;
+  double rx=0,ry=0;
+  double rad=0; 
+  double aplus=0, wrx=0, wry=0;
+
+
+rx = absX - xc;
+ry = absY - yc;
+if (rx == 0) {
+    rad = Math.abs(ry);
+    if (ry > 0) {
+    	rtnAng = 90;
+    } else {
+    	rtnAng = 270;
+    }
+    radius = rad;
+    return rtnAng;
+} // (rx = 0)
+
+if (ry == 0) {
+    rad = Math.abs(rx);
+    if (rx > 0) {
+    	rtnAng = 0;
+    } else  {
+    	rtnAng = 180;
+    }
+    radius = rad;
+    return rtnAng;
+} // (ry = 0)
+
+rad = (Math.abs(rx) * Math.abs(rx)) + (Math.abs(ry) * Math.abs(ry));
+rad = Math.sqrt(rad);
+radius = rad;
+
+    if ((rx > 0) && (ry > 0)) {
+        aplus = 0; 
+    	wrx = rx; 
+    	wry = ry;
+    } // (rx > 0 And ry > 0)
+    if ((rx < 0) && (ry > 0)) {
+        aplus = 90; 
+        wrx = ry; 
+        wry = rx;
+    } // (rx < 0 And ry > 0)
+    if ((rx < 0) && (ry < 0)) { 
+        aplus = 180; 
+        wrx = rx; 
+        wry = ry;
+    } // (rx < 0 And ry < 0)
+    if ((rx > 0) && (ry < 0)) {
+        aplus = 270; 
+        wrx = ry; 
+        wry = rx;
+    } // (rx > 0 And ry < 0)
+
+   rtnAng = Math.abs(wry) / Math.abs(wrx); // angle if not 0/90/180/270
+   rtnAng = Math.atan(rtnAng);
+   rtnAng = (rtnAng / 3.141592654) * 180;  // rad2deg
+   rtnAng = rtnAng+aplus;
+   if (rtnAng > 360) { rtnAng = rtnAng-360;}
+
+   return rtnAng;
+} // find_Ang_Rad
+
+	
+	
+} // convertCls
---
+++ dxf12objects.java
@@ -0,0 +1,607 @@
+package cf2dxf;
+
+/**
+  *
+  * Description
+  *
+  * @version 1.0 from 7/30/2016
+  * @author Tim Gathercole
+  */
+
+public class dxf12objects {
+  
+  // start attributes
+  public double xabs = 0;
+  public double yabs = 0;
+  public int hndl = 1000;
+  // 1,1 = Right hand, pointing up Flap
+  public int Xaxis = 1; // Direction of X axis +1 or -1
+  public int Yaxis = 1; // Direction of Y axis +1 or -1
+   
+   
+  public String dxf = "";
+  public String dxfxmax = "10";
+  public String dxfymax = "10";
+  public String dimasz = "5.5";
+  public String dimtxt = "7.1";
+  public String dimexe = "2.2";
+  public String lunits = "2";
+  public String luprec = "4";
+  public String dimdec = "9";
+  public String dimzin = "8";
+  public String dimexo = "0.1";
+  public String dimgap = "0.1";
+  // end attributes
+  
+  // start methods
+  protected String dxf_header12() {
+    // DXF Version 12 Header - Simplified version
+    String cr = "\r\n";
+    
+    dxf +="  0"+ cr;
+    dxf +="SECTION"+ cr;
+    dxf +="  2"+ cr;
+    dxf +="HEADER"+ cr;
+    dxf +="  9"+ cr;
+    dxf +="$ACADVER"+ cr;
+    dxf +="  1"+ cr;
+    dxf +="AC1009"+ cr; // DXF Version 12
+    dxf +="  9"+ cr;
+    dxf +="$EXTMIN"+ cr; // X, Y, and Z drawing extents 10, 20, 30 lower-left corner (in WCS)
+    dxf +=" 10"+ cr;
+    dxf +="0"+ cr;
+    dxf +=" 20"+ cr;
+    dxf +="0"+ cr;
+    dxf +="  9"+ cr;
+    dxf +="$EXTMAX"+ cr; // X, Y, and Z drawing extents 10, 20, 30 upper-right corner (in WCS)
+    dxf +=" 10"+ cr;
+    dxf += dxfxmax + cr;
+    dxf +=" 20"+ cr;
+    dxf += dxfymax + cr;
+    dxf +="  0"+ cr;
+    dxf +="ENDSEC"+ cr;
+    dxf +="  0"+ cr;
+    dxf +="SECTION"+ cr;
+    dxf +="  2"+ cr;
+    dxf +="TABLES"+ cr;
+    dxf +="  0"+ cr;
+    
+dxf +="TABLE"+ cr;
+dxf +="  2"+ cr;
+dxf +="LAYER"+ cr;
+dxf +=" 70"+ cr;
+dxf +="    14"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="0"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="     7"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="DIMS"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="     7"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="SAFETYEDGE"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="    11"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="CUT"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="     1"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="CREASE"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="     3"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="PERF"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="     2"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="SCORE"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="     5"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="MATRIX"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="   126"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="ZIPPER"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="     8"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="CUTCRE"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="     6"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="DRAWNOTBURN"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="   254"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="BURNNORULE"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="    32"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="PUNCH"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="   240"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="LAYER"+ cr;
+dxf +="  2"+ cr;
+dxf +="OTHER"+ cr;
+dxf +=" 70"+ cr;
+dxf +="     0"+ cr;
+dxf +=" 62"+ cr;
+dxf +="     7"+ cr;
+dxf +="  6"+ cr;
+dxf +="CONTINUOUS"+ cr;
+dxf +="  0"+ cr;
+dxf +="ENDTAB"+ cr;
+  
+    dxf +="  0"+ cr;
+    dxf +="ENDSEC"+ cr;
+    dxf +="  0"+ cr;
+    dxf +="SECTION"+ cr;
+    dxf +="  2"+ cr;
+    dxf +="BLOCKS"+ cr;
+    dxf +="  0"+ cr;
+    dxf +="ENDSEC"+ cr;
+    dxf +="  0"+ cr;
+    dxf +="SECTION"+ cr;
+    dxf +="  2"+ cr;
+    dxf +="ENTITIES"+ cr;
+    
+    return dxf;
+  }
+  
+  
+  protected String dxf_footer12() {
+    /* WONT PRINT */
+    //  Final part of the DXF File
+    //  DXF version 12
+    String cr = "\r\n"; // Chr(10).Chr(13);
+    
+    dxf +="  0" + cr;
+    dxf +="ENDSEC" + cr;
+    dxf +="  0" + cr;
+    dxf +="EOF" + cr;
+    
+    return dxf;
+  }
+  
+  protected boolean Line(double xval, double yval, String layer) {
+    // DXF version 12
+    // xval    = Incremental X movement
+    // yval    = Incremental Y movement
+    // layer   = Layer to use
+    // eColor    = Colour to use
+    // eLinetype = Line type to use
+    //echo xval.'*'.yval.'*'.layer.'*'.eColor.'*'.eLinetype.'<br>';
+    String cr = "\r\n";
+    String layCol[] = this.getLayer(layer);
+    
+    dxf +="  0" + cr;
+    dxf +="LINE" + cr;
+    dxf +="  5" + cr; //  Drawing Datadbase Handle
+    dxf +=hndl + cr;
+    dxf +="  8" + cr; //  Layer name
+    dxf += layCol[0] + cr; // layer + cr;
+    
+    dxf +=" 6" + cr; // Line Type
+    dxf +="CONTINUOUS" + cr;
+    dxf +=" 62" + cr; // Line Colour
+    dxf += layCol[1] + cr; // getLineCol(layer) + cr;
+    
+    dxf +=" 10" + cr; //  Start X
+    dxf += xabs + cr;
+    dxf +=" 20" + cr; //  Start Y
+    dxf += yabs + cr;
+    dxf +=" 30" + cr; //  Start Z
+    dxf +="0.0" + cr;
+    dxf +=" 11" + cr; //  End X
+    dxf += String.valueOf(xabs + xval * Xaxis) + cr;
+    dxf +=" 21" + cr; //  End Y
+    dxf += String.valueOf(yabs + yval * Yaxis) + cr;
+    dxf +=" 31" + cr; //  End Z
+    dxf +="0.0" + cr;
+    //  Finish
+    xabs = xabs + xval * Xaxis;
+    yabs = yabs + yval * Yaxis;
+    hndl = hndl + 1;
+    return false;
+  }
+  
+  
+  protected String[] getLayer(String layer) {
+    // Return a Layer Name from the number
+    /* 
+
+1 Cut
+2 Crease
+3 Perforation (2 parameters, cut and gap)
+4 Score/half cut.
+40 Rillma/Matrix designs.
+41 Zipper (3 parameters, length, gap, and angle).
+42 Cut/Crease (3 parameters, cut, crease, and land length).
+43 Draw but don’t burn into the die.
+44 Burn but don’t rule.
+45 Safety edge (2 parameters, height, and pitch).
+46 Dimensions.
+99 Punch lines representing a shape that the punch would be expected to cut.
+    */
+    String rtn[] = {"CUT","1"};
+    switch (layer) {
+      case "1" : 
+        rtn[0] = "CUT";  // Artios understands Colour 7 as CUT   
+        rtn[1] = "1";
+        break;
+      case "2" : 
+        rtn[0] = "CREASE";  
+        rtn[1] = "3";
+        break;
+      case "3" : 
+        rtn[0] = "PERF";
+        rtn[1] = "2";
+        break; 
+      case "4" : 
+        rtn[0] = "SCORE";
+        rtn[1] = "5";
+        break;       
+      case "40" : 
+        rtn[0] = "MATRIX";
+        rtn[1] = "126";
+        break; 
+      case "41" : 
+        rtn[0] = "ZIPPER";
+        rtn[1] = "8";
+        break;  
+      case "42" : 
+        rtn[0] = "CUTCRE";
+        rtn[1] = "6";
+        break;  
+      case "43" : 
+        rtn[0] = "DRAWNOTBURN";
+        rtn[1] = "32";
+        break;  
+      case "44" : 
+        rtn[0] = "BURNNORULE";
+        rtn[1] = "254";
+        break;  
+      case "45" : 
+        rtn[0] = "SAFETYEDGE";
+        rtn[1] = "11";
+        break;           
+      case "46" : 
+        rtn[0] = "DIMS";
+        rtn[1] = "7";
+        break;          
+      case "99" : 
+        rtn[0] = "PUNCH";
+        rtn[1] = "240";
+        break;          
+      default: 
+        rtn[0] = "OTHER";
+        rtn[1] = "7";
+System.out.println(layer);
+    } // end of switch
+    return rtn;  
+  }
+  
+  protected boolean circle(double XcenterAbs, double YcenterAbs, double radius, String layer, int eColor) {
+    /*
+    * Make a relative move to the center of the circle and call this function
+    */
+    String cr = "\r\n";
+    String layCol[] = this.getLayer(layer);
+    
+    dxf +="  0"+ cr;
+    dxf +="CIRCLE"+ cr;
+    
+    dxf +="  5"+ cr; //  Drawing Datadbase Handle
+    dxf += hndl+ cr;
+    dxf +="  8"+ cr; //  Layer name
+    dxf += layCol[0] + cr; //layer+ cr;
+    
+    dxf +=" 6" + cr; // Line Type
+    dxf +="CONTINUOUS" + cr;
+    dxf +=" 62" + cr; // Line Colour
+    dxf += layCol[1] + cr; // getLineCol(layer) + cr;
+    
+    dxf +=" 10"+ cr; //  center X
+    dxf += XcenterAbs + cr;
+    dxf +=" 20"+ cr; //  center Y
+    dxf += YcenterAbs + cr;
+    dxf +=" 30"+ cr; //  center Z
+    dxf +="0.0"+ cr;
+    dxf +=" 40"+ cr; //  Radius
+    dxf +=radius+ cr;
+    
+    //  Finish
+    hndl = hndl + 1;
+    
+    return true;
+  }
+  
+  /**
+  * 
+  * @param XcenterAbs
+  * @param YcenterAbs
+  * @param radius
+  * @param startAngle (0-90-180-270) Mid(3) not Top Str
+  * @param endAngle
+  * @param endPosX - Incremental movement to update AbsX
+  * @param endPosY - to update AbsY
+  * @param layer
+  * @return DXF Arc
+  */ 
+  protected boolean arc2(double XcenterAbs, double YcenterAbs, double radius, double startAngle, double endAngle, double endPosX, double endPosY, String layer) {
+    String cr = "\r\n";
+    String layCol[] = this.getLayer(layer);
+    
+    dxf +="  0" + cr;
+    dxf +="ARC" + cr;
+    
+    dxf +="  5" + cr; //  Drawing Datadbase Handle
+    dxf += Integer.toString(hndl) + cr;
+    dxf +="  8" + cr; //  Layer name
+    dxf += layCol[0] + cr; //layer + cr;
+    
+    dxf +=" 6" + cr; // Line Type
+    dxf +="CONTINUOUS" + cr;
+    dxf +=" 62" + cr; // Line Colour
+    dxf += layCol[1] + cr; // getLineCol(layer) + cr;
+    
+    dxf +=" 10" + cr; //  center X
+    dxf += XcenterAbs + cr;
+    dxf +=" 20" + cr; //  center Y
+    dxf += YcenterAbs + cr;
+    
+    dxf +=" 30" + cr; //  center Z
+    dxf +="0.0" + cr;
+    
+    dxf +=" 40" + cr; //  Radius
+    dxf +=radius + cr;
+    
+    dxf +=" 50" + cr; //  Start Angle of Arc
+    dxf +=startAngle + cr;
+    dxf +=" 51" + cr; //  End Angle of Arc
+    dxf +=endAngle + cr;
+    
+    //  Finish
+    xabs = xabs + endPosX;
+    yabs = yabs + endPosY;
+    hndl += 1;
+    
+    return false;
+  }
+  
+  
+  protected double[] getArcAngles(double IncMoveX, double IncMoveY, double radius, int cw) {
+    /* Partial conversion from HPGL2DXF - Largely a Special case protected boolean for 0422 slots
+    * Find
+    * Xcenter, Ycenter
+    * startAngle, endAngle
+    */
+    // array('Xcenter' => 0, 'Ycenter' => 0, 'startAngle' => 0, 'endAngle' => 0) ** Java doesn't support associative arrays **
+    double rtnArray[] = {0, 0, 0, 0};
+    
+    double rx =  (IncMoveX / 2) * Xaxis ; // absX - Xc
+    double ry =  (IncMoveY / 2) * Yaxis; // absY - Yc
+    double ang = -1; // set-up
+    double aplus = 0; // set-up
+    double angle = 0; // not required?
+    double wrx = 0;
+    double wry = 0;
+    double tmp = 0;
+    double strang = 0;
+    double endang = 0;
+    
+    if (rx == 0) {
+      angle = 180; // simple 180 arc
+      //  rad = Abs(ry);
+      if (ry > 0) {
+        ang = 270; //90;
+      } else {
+        ang = 90; //270;
+      }
+      //GoTo arnxt
+    } else if (ry == 0) {
+      angle = 180; // simple 180 arc
+      // rad = Abs(rx)
+      if (rx > 0) {
+        ang = 180; //0;
+      } else {
+        ang = 0; //180;
+      }
+      // GoTo arnxt
+      //  rad = (Abs(rx) * Abs(rx)) + (Abs(ry) * Abs(ry)) // *** Radius is Known in this instance ***
+      //  rad = Sqr(rad)
+      
+    } else if (rx > 0 && ry > 0) {
+      aplus = 0;
+      wrx = rx;
+      wry = ry;
+    } else if (rx < 0 && ry > 0) {
+      aplus = 90;
+      wrx = ry;
+      wry = rx;
+    } else if (rx < 0 && ry < 0) {
+      aplus = 180;
+      wrx = rx;
+      wry = ry;
+    } else if (rx > 0 && ry < 0) {
+      aplus = 270;
+      wrx = ry;
+      wry = rx;
+    }
+    
+    if (ang == -1) { // not touched by the first 2 conditions
+      ang = Math.abs(wry) / Math.abs(wrx);  /* angle if not 0/90/180/270 */
+      ang = Math.atan(ang);
+      ang = (ang / 3.141592654) * 180;  /* rad2deg */
+    }
+    //arnxt:
+    //  radius = rad
+    
+    strang = ang + aplus;
+    endang = strang + angle;
+    if (endang > 360) {
+      endang = endang - 360;
+    }
+    
+    /* start_angle = (strang * 3.141592654) /180  deg2rad */
+    /* end_angle = (endang * 3.141592654) /180  deg2rad */
+    if (Yaxis == -1) { // flipped slot
+      tmp = strang;
+      strang = endang;
+      endang = tmp;
+    }
+    
+    rtnArray[0] = rx; // Xcenter
+    rtnArray[1] = ry; // Ycenter
+    rtnArray[2] = strang; // startAngle
+    rtnArray[3] = endang; // endAngle
+    
+    return rtnArray;
+  } // getArcAngles
+
+
+/**
+   * 
+   * @param xval
+   * @param yval
+   * @param textStr
+   * @param layer
+   * @return
+   */
+  protected boolean TextInsert(double xval, double yval, String textStr, String textSz, String layer) {
+    // DXF version 12
+    // xval    = Incremental X movement
+    // yval    = Incremental Y movement
+    // layer   = Layer to use
+    // eColor    = Colour to use
+    // eLinetype = Line type to use
+    String cr = "\r\n";
+    String layCol[] = this.getLayer(layer);
+
+    dxf +="  0" + cr;
+    dxf +="TEXT" + cr;
+    dxf +="  5" + cr; //  Drawing Datadbase Handle
+    dxf += hndl + cr;
+    dxf +="  8" + cr; //  Layer name
+    dxf += layCol[0] + cr; // layer + cr;
+    
+    dxf +=" 6" + cr; // Line Type
+    dxf +="CONTINUOUS" + cr;
+    dxf +=" 62" + cr; // Line Colour
+    dxf += layCol[1] + cr;
+    
+    dxf +=" 10" + cr; //  Start X
+    dxf += String.valueOf(xval) + cr;
+    dxf +=" 20" + cr; //  Start Y
+    dxf += String.valueOf(yval) + cr;
+//    dxf +=" 30" + cr; //  Start Z
+//    dxf +="0.0" + cr;
+    dxf +=" 40" + cr; //  End X
+    dxf += String.valueOf(textSz) + cr;
+    
+    dxf +=" 41" + cr; // Line Type
+    dxf +="0" + cr;
+    dxf +=" 50" + cr; // Line Type
+    dxf +="0" + cr;    
+    
+    dxf +=" 1" + cr; //  
+    dxf += textStr + cr;
+    dxf +=" 72" + cr;
+    dxf +="0" + cr;    
+    //  Finish
+//    xabs = xabs + xval * Xaxis; Don
+//    yabs = yabs + yval * Yaxis;
+    hndl = hndl + 1;
+    return false;
+  } // TextInsert  
+
+  
+  
+  
+  
+  
+  
+  // end methods
+} // end of dxf12objects
